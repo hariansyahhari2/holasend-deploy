579d9a9f8908f29e4ab013f3a946f1cedbf9ea33
unknown <nurarifinmuhammad@gmail.com>
2021-04-04 22:21:28 +0700

    Formatted Date

--- api-holasend/src/main/java/com/enigmacamp/api/holasend/configs/exporter/ReportModelMapper.java
+++ api-holasend/src/main/java/com/enigmacamp/api/holasend/configs/exporter/ReportModelMapper.java
@@ -8,6 +8,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReportModelMapper {
+
+    private static String convertDateOrTime(String input, String replacement) {
+        input = input.replaceAll("-", replacement);
+        return input;
+    }
+
+    private static String dateFormat(String formattedDateTime) {
+        String date = formattedDateTime.substring(0, 10);
+        date = convertDateOrTime(date, "/");
+        String time = formattedDateTime.substring(11, 19);
+        time = convertDateOrTime(time, ":");
+        formattedDateTime = date + " " + time;
+        return formattedDateTime;
+    }
+
     public static List<TaskReportModel> convert(List<Task> taskList) {
         List<TaskReportModel> modelList = new ArrayList<>();
 
@@ -40,7 +55,7 @@ public class ReportModelMapper {
                     if (task.getPickUpTime() != null) {
                         DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
                         String formattedDateTime = task.getPickUpTime().format(formatter);
-
+                        formattedDateTime = dateFormat(formattedDateTime);
                         model.setPickUpTime(formattedDateTime);
                     } else {
                         model.setPickUpTime("");
@@ -49,6 +64,7 @@ public class ReportModelMapper {
                     if (task.getDeliveredTime() != null) {
                         DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
                         String formattedDateTime = task.getDeliveredTime().format(formatter);
+                        formattedDateTime = dateFormat(formattedDateTime);
                         model.setDeliveredTime(formattedDateTime);
                     } else {
                         model.setDeliveredTime("");
@@ -59,6 +75,7 @@ public class ReportModelMapper {
                         String formattedDateTime = "";
                         if (task.getCourierActivity().getReturnTime() != null) {
                             formattedDateTime = task.getCourierActivity().getReturnTime().format(formatter);
+                            formattedDateTime = dateFormat(formattedDateTime);
                         }
                         model.setReturnTime(formattedDateTime);
                     } else {
@@ -68,7 +85,11 @@ public class ReportModelMapper {
                     model.setStatus(task.getStatus().toString());
                     model.setPriority(task.getPriority().toString());
                     model.setNotes(task.getNotes());
-                    model.setCreateDate(task.getCreateDate().toString());
+
+                    DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+                    String formattedDateTime = task.getCreateDate().format(formatter);
+                    formattedDateTime = dateFormat(formattedDateTime);
+                    model.setCreateDate(formattedDateTime);
                     modelList.add(model);
                 }
         );
